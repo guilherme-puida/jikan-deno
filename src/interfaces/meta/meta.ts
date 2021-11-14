cecac4dd9e8b1c5f7ec7261c1743a1d158d93c0e
Guilherme Puida <guilhermepmoreira00@gmail.com>
2021-11-14 02:04:01 -0300

    :construction: Added interface for meta request.
    The meta/requests endpoint just return a regular json, so there's no need for an interface

---
+++ src/interfaces/meta/meta.ts
@@ -0,0 +1,8 @@
+export interface Status {
+    cached_requests: number;
+    connected_clients: string;
+    requests_this_month: number;
+    requests_this_week: number;
+    requets_today: number;
+    total_connectios_received: number;
+}
